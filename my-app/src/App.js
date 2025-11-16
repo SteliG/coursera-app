4953ca86efb377a6bf45b981baf2ab2121c8edf4
SteliG <stiliana.yordanova@gmail.com>
2025-11-17 00:46:14 +0200

    added header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <div>Homepage</div>
-    </div>
+    <>
+    <Header/>
+      </>
+     
   );
 }
 
