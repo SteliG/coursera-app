cf21686178757759b6153d205eae5c90e1289e7f
SteliG <stiliana.yordanova@gmail.com>
2025-11-18 21:21:58 +0200

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Header from './Header';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <>
-    <Header/>
-      </>
-     
+      <Header/>
+      <Main/>
+      <Menu/>
+      <Footer/>
+    </>
   );
 }
 
